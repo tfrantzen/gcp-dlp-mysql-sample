b0f5311298dbcf37ac5bafda9fc0c0d3b103b797
tfrantzen <tfrantzen@google.com>
2017-12-21 13:45:57 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,30 @@
 'use strict';
  
- const google_dlp_api = require('@google-cloud/dlp');
- const mysql = require('mysql');
+ // Import requirements
+ const dlpConnector = require('./dlpConnector');
+ const mysqlConnector = require('./mysqlConnector');
  const nconf = require('nconf');
-
+ 
+ // Import MySQL DB arguments for key.json file
+ nconf.argv().env().file('keys.json');
+ const user = nconf.get('mysqlUser');
+ const pwd = nconf.get('mysqlPwd');
+ const host = nconf.get('mysqlHost');
+ const db = nconf.get('mysqlDb');
+ const tbl = nconf.get('mysqlTable');
+ 
+ // define the parameters for the Google DLP API
  const MIN_LIKELIHOOD = 'LIKELIHOOD_UNSPECIFIED';
  const MAX_FINDINGS = '100000';
+ // What type of PII are you looking for?
  const INFO_TYPES = [
    {'name':'US_SOCIAL_SECURITY_NUMBER'},
    {'name':'EMAIL_ADDRESS'}
  ];
- const INCLUDE_QUOTES = true;
-
+ // Include value in response
+ const INCLUDE_QUOTES = true;  
+ 
+ // Convert MySQL response into DLP table object
  function createDlpTableRequest(mysqlResponse){
    var table = { headers:[], rows:[] };
 
@@ -46,70 +59,11 @@
    return table;
  }
 
- function executeQuery(host, db_name, user, pwd, query, cb){
-   // create connection to mysql db  
-   var connection = mysql.createConnection({
-     host: host,
-     user: user,
-     password: pwd,
-     database: db_name
-   });
-
-   // query the mysql table for query
-   connection.query(
-     query,
-     (err, results) => {
-       if (err) {
-         throw err;
-       }
-
-       cb(results);
-     }
-   );
-   connection.end();
- }
-
- function inspectTable(table, minLikelihood, maxFindings, infoTypes, includeQuote, cb) {
-   // Instantiates a client
-   const dlp = new google_dlp_api.DlpServiceClient();
-
-   // Construct items to inspect
-   const items = [{'table':table}];
-
-   // Construct request
-   const request = {
-     inspectConfig: {
-       infoTypes: infoTypes,
-       minLikelihood: minLikelihood,
-       maxFindings: maxFindings,
-       includeQuote: includeQuote,
-     },
-     items: items,
-   };
-
-   // Send request to DLP api
-   dlp.inspectContent(request).then(response => {
-     // first result is the response
-     const dlp_table_result = response[0].results[0].findings;
-
-   cb(dlp_table_result);
-   }).catch(err => {
-     throw err;
-     });
- }
-
- nconf.argv().env().file('keys.json');
- const user = nconf.get('mysqlUser');
- const pwd = nconf.get('mysqlPwd');
- const host = nconf.get('mysqlHost');
- const db = nconf.get('mysqlDb');
- const tbl = nconf.get('mysqlTable');
-
- executeQuery(host, db, user, pwd, 'SELECT * FROM ' + tbl, function(mysql_response) {
+ mysqlConnector.executeQuery(host, db, user, pwd, 'SELECT * FROM ' + tbl, function(mysql_response) {
    // dlp request
    var dlp_table_request = createDlpTableRequest(mysql_response);
 
-   inspectTable(dlp_table_request, MIN_LIKELIHOOD, MAX_FINDINGS, INFO_TYPES, INCLUDE_QUOTES, (dlp_findings) => {
+   dlpConnector.inspectTable(dlp_table_request, MIN_LIKELIHOOD, MAX_FINDINGS, INFO_TYPES, INCLUDE_QUOTES, (dlp_findings) => {
      // Write out results
      console.log(dlp_findings);
    });
